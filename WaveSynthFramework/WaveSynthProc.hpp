11b599df9f47ad67a352314dd955d79a18169679
Eric George <eegee.dev@gmail.com>
2016-04-23 19:56:07 -0700

    Updated to simple oscillator

---
+++ WaveSynthFramework/WaveSynthProc.hpp
@@ -0,0 +1,14 @@
+//
+//  WaveSynthProc.hpp
+//  AUInstrument
+//
+//  Created by Eric on 4/23/16.
+//  Copyright © 2016 Eric George. All rights reserved.
+//
+
+#ifndef WaveSynthProc_hpp
+#define WaveSynthProc_hpp
+
+#include <stdio.h>
+
+#endif /* WaveSynthProc_hpp */
